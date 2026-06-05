4e43a4af6840e3b90b3146308bc4e163271800ca
AidaDehdastt <aida.dehdast.1384@gmail.com>
2026-06-05 10:17:06 +0200

    Fixat routing och uppdaterat projekt

--- cv-portal-react/src/main.jsx
+++ cv-portal-react/src/main.jsx
@@ -5,10 +5,9 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/cv-portal-react">
-       <App />
-    </BrowserRouter>
-
-  </React.StrictMode>
+<React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</React.StrictMode>
 );
